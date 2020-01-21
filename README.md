450dac54376900dab24c2cd74ab7d7305e288716
David Adams <dladams@bnl.gov>
2020-01-21 09:52:14 -0600

    Make useful.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # duneproc
+
+David Adams  
+January 2020
+
+Package to facilitate the running of `lar` jobs with `dunetpc`.
+
+The command `processData` runs a `lar` job with the first argument
+specifying the fcl configuration and the second the data to be processed.
+For help, use
+  processData -H
---
+++ setup.sh
@@ -0,0 +1,23 @@
+# setup.sh
+#
+# David Adams
+# January 2020
+
+if [ -n "$DUNEPROC_DIR" ]; then
+  echo It appears duneproc is already set up:
+  echo DUNEPROC_DIR = $DUNEPROC_DIR
+
+elif [ -z "$DUNETPC_VERSION" ]; then
+  echo "Please set up dunetpc."
+
+else
+
+DUNEPROC_DIR=$(dirname $(readlink -f $BASH_SOURCE))
+PATH=$DUNEPROC_DIR/bin:$PATH
+TMPPATH=.:./job:
+if [ ${FHICL_FILE_PATH:0:8} = $TMPPATH ]; then FHICL_FILE_PATH=${FHICL_FILE_PATH:8}; fi
+FHICL_FILE_PATH=${TMPPATH}$DUNEPROC_DIR/fcl:$FHICL_FILE_PATH
+unset TMPPATH
+
+fi
+
