21b87094adf3c072fec4f409c91adb1668a67575
David Adams <dladams@bnl.gov>
2020-03-21 13:09:15 -0500

    Update warning message in directSetup.

--- directSetup.sh
+++ directSetup.sh
@@ -3,6 +3,14 @@
 # David Adams
 # January 2020
 
+ERRMSG='***********************************************
+This is the direct packages setup.
+It is preferable to build with ./build and then
+run setup from the installation directory.
+***********************************************'
+
+echo "$ERRMSG"
+
 if [ -n "$DUNEPROC_DIR" ]; then
   echo It appears duneproc is already set up:
   echo DUNEPROC_DIR = $DUNEPROC_DIR
@@ -12,16 +20,14 @@ elif [ -z "$DUNETPC_VERSION" ]; then
 
 else
 
-echo Setting up duneproc
-DUNEPROC_DIR=$(dirname $(readlink -f $BASH_SOURCE))
-PATH=$DUNEPROC_DIR/bin:$PATH
-TMPPATH=.:./job:
-if [ ${FHICL_FILE_PATH:0:8} = $TMPPATH ]; then FHICL_FILE_PATH=${FHICL_FILE_PATH:8}; fi
-FHICL_FILE_PATH=${TMPPATH}$DUNEPROC_DIR/fcl:$FHICL_FILE_PATH
-unset TMPPATH
+  echo Setting up duneproc
+  DUNEPROC_DIR=$(dirname $(readlink -f $BASH_SOURCE))
+  PATH=$DUNEPROC_DIR/bin:$PATH
+  TMPPATH=.:./job:
+  if [ ${FHICL_FILE_PATH:0:8} = $TMPPATH ]; then FHICL_FILE_PATH=${FHICL_FILE_PATH:8}; fi
+  FHICL_FILE_PATH=${TMPPATH}$DUNEPROC_DIR/fcl:$FHICL_FILE_PATH
+  unset TMPPATH
 
 fi
 
-echo Package has been set up directly.
-echo It is preferable to build with ./build and then
-echo run setup from the installation directory.
+echo "$ERRMSG"
